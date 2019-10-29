1f6e727448b4a06a55a8dd156e28a178980a810e
kevindhanna <kevindhanna@gmail.com>
2019-10-29 14:27:59 +0000

    semi functional

--- jasmine_standalone/spec/thingSpec.js
+++
@@ -1,8 +0,0 @@
-// it('does things', function(){
-//   expect(4).toEqual(5)
-
-// })
-
-describe('other things', function() {
-  it('does some things')
-})
---
+++ spec/thingSpec.js
@@ -0,0 +1,30 @@
+Describe('thing', function() {
+  It('does some stuff', function() {
+    Demand(4).toBe(4)
+  })
+})
+Describe('thing1', function() {
+  It('does some stuff1', function() {
+    Demand(4).toBe(4)
+  })
+})
+Describe('thing2', function() {
+  It('does some stuff2', function() {
+    Demand(4).toBe(5)
+  })
+})
+Describe('thing3', function() {
+  It('does some stuff3', function() {
+    Demand(4).toBe(4)
+  })
+})
+Describe('thing4', function() {
+  It('does some stuff4', function() {
+    Demand(4).toBe(4)
+  })
+})
+Describe('thing5', function() {
+  It('does some stuff5', function() {
+    Demand(4).toBe(5)
+  })
+})
